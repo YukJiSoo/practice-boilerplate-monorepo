19a196698b064a3e6ccf98cf4b706f052a91c290
YukJiSoo <wltn3231@naver.com>
2019-11-14 17:47:16 +0900

    feat: 클라이언트에서 요청 경로

--- cocode/src/App.js
+++ cocode/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_SERVER_ADDRESS = 'http://localhost:4000/api';
+const API_SERVER_ADDRESS = 'http://106.10.32.126:3030/api';
 const FETCH_OPTION = {
 	crossDomain: true,
 	method: 'GET',
