a8b7f636cfa947aec836d149208ebc505d8967c8
YukJiSoo <wltn3231@naver.com>
2019-11-17 17:43:32 +0900

    deploy script 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ echo "deploy service: ${SERVICE}"
 
 cd practice-boilerplate-monorepo/$SERVICE
 
-if [SERVICE = "cocode"]; then
+if ["${SERVICE}" = "cocode"]; then
     sshpass -p "${COCODE_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${COCODE_IP}" echo "cocode"
 elif [SERVICE = "api-server"]; then
     sshpass -p "${API_SERVER_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${API_SERVER_IP}" echo "api-server"
