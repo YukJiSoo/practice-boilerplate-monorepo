7b50183858176a93949b5a04ca5522b2ab8012ce
YukJiSoo <wltn3231@naver.com>
2019-11-17 15:57:16 +0900

    일단은 되는 방법으로

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,3 +4,8 @@ SERVICE="$1"
 echo "deploy service: ${SERVICE}"
 
 cd $SERVICE
+
+docker pull minyoung403/catch-my-mind-front
+docker stop catch-my-mind-front
+docker rm catch-my-mind-front
+docker run --name=catch-my-mind-front -d -p 80:80 minyoung403/catch-my-mind-front
