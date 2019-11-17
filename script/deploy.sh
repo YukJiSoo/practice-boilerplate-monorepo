3d0c96a74d5a9dcdab823e186426135999628165
YukJiSoo <wltn3231@naver.com>
2019-11-17 17:39:29 +0900

    deploy server request test

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-echo "$(pwd)"
-
 SERVICE="$1"
 echo "deploy service: ${SERVICE}"
 
 cd practice-boilerplate-monorepo/$SERVICE
-pwd
 
-echo "$(pwd)"
+if [SERVICE = "cocode"]; then
+    sshpass -p "${COCODE_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${COCODE_IP}" echo "cocode"
+elif [SERVICE = "api-server"]; then
+    sshpass -p "${API_SERVER_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${API_SERVER_IP}" echo "api-server"
+else
+    echo "${SERVICE} nono!"
+fi
