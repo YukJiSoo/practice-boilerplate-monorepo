8b598e8dd74b509a15c63de85ade7e9ae6b703b4
YukJiSoo <wltn3231@naver.com>
2019-11-17 15:45:56 +0900

    script permission setting

--- script/accept-http-in-docker.sh
+++ script/accept-http-in-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "set docker accept http network..."
+
 'sudo cat /etc/docker/daemon.json | jq ''. + {"insecure-registries": ["45.119.146.88:5000"]}''
     | sudo tee /etc/docker/daemon.json'
 sudo systemctl restart docker
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+
+service="$1"
+echo "deploy service: $1"
+echo "variable service: ${service}"
