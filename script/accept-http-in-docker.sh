68966c93e51260be1303a8d38d3130f7177d74b0
YukJiSoo <wltn3231@naver.com>
2019-11-17 18:08:25 +0900

    왜안대지

--- script/accept-http-in-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "set docker accept http network..."
-sudo cat /etc/docker/daemon.json
-sudo cat /etc/docker/daemon.json | jq . + {"insecure-registries": ["45.119.146.88:5000"]} | sudo tee /etc/docker/daemon.json
-sudo cat /etc/docker/daemon.json
-sudo systemctl restart docker
-
-sudo cat /etc/docker/daemon.json
