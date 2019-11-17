ae861ec68cbe3e26d007fe3b91baf2d18fb9ccc2
YukJiSoo <wltn3231@naver.com>
2019-11-17 18:32:03 +0900

    바보

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,6 +4,7 @@ SERVICE="$1"
 echo "deploy service: ${SERVICE}"
 
 if [ "${SERVICE}" = "cocode" ]; then
+    echo "deploy service: ${COCODE_IP}"
     sshpass -p "${COCODE_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${COCODE_IP}" echo "cocode"
 elif [ "${SERVICE}" = "api-server" ]; then
     sshpass -p "${API_SERVER_PASSWORD}" ssh -o StrictHostKeyChecking=no root@"${API_SERVER_IP}" echo "api-server"
